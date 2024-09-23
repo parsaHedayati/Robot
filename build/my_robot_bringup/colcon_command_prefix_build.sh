7b1c67ea6d516ecb8449508207fb0e7f7483770a
Parsa <parsa@ubuntu.myguest.virtualbox.org>
2024-09-23 13:49:13 +0300

    ros2_urdf_ws

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/parsa/ros2_urdf_ws/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ install/my_robot_description/share/my_robot_description/cmake/my_robot_descriptionConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/parsa/ros2_urdf_ws/build/my_robot_description/ament_cmake_core/my_robot_descriptionConfig-version.cmake
