d2a917a69f750db38a3c535e06263526b950cdc3
parsaHedayati <72393420+parsaHedayati@users.noreply.github.com>
2025-01-05 16:37:38 +0200

    Update command_publisher.py

--- src/inverse_kinematics/inverse_kinematics/command_publisher.py
+++ src/inverse_kinematics/inverse_kinematics/command_publisher.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# command_publisher.py
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
@@ -19,6 +19,7 @@ def main(args=None):
     rclpy.init(args=args)
     node = CommandPublisher()
 
+    
     node.send_command("stand")
 
     rclpy.spin(node)
